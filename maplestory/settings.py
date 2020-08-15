fae45edc6b794386254a40f88a5cf8e94b05657e
iam-dante <koreanhoseon@gmail.com>
2020-08-15 19:27:41 +0900

    settings.py에 설치된 앱 추가

--- maplestory/settings.py
+++ maplestory/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-
+    'wiki.apps.WikiConfig',
 ]
 
 MIDDLEWARE = [
@@ -48,7 +48,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'wiki.apps.WikiConfig',
 ]
 
 ROOT_URLCONF = 'maplestory.urls'
