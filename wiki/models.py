013df72f036c5339b895945629221a3d7f1426d2
iam-dante <koreanhoseon@gmail.com>
2020-08-15 20:18:43 +0900

    모델링 임시

--- wiki/models.py
+++ wiki/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+# user(미리 GitHub 연동 작업까지 생각), title, content, hits(조회수), Revision, comment, Like or Dislike
+class Term(models.Model):
+    user = models.CharField(max_length=20)
+    term_title = models.TextField()
+    term_content = models.TextField()
+    # hits = 
+
+class Comment(models.Model):
+    comment_date = models.DateField(auto_now_add=True)
+    comment_content = models.CharField(max_length=200)
+    # comment_writer = 
