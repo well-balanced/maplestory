058ea96a96abb1895d567e493c83106bea12aebc
sangminpark <qpwpep5429@gmail.com>
2020-08-15 19:55:38 +0900

    test

--- wiki/views.py
+++ wiki/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def write(request):
+    return render(request, 'write.html')
+
+def detail(request, id):
+    return render(request, 'term.html')
+
+def 
