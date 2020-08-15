ec2953aef006d34b2009601426c901497db30d5c
iam-dante <koreanhoseon@gmail.com>
2020-08-15 20:29:24 +0900

    Remove unnecessary code snipets.

--- wiki/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from wiki.views import test, testt
-
-urlpatterns = [
-    path('', test, ),
-    path('<int:id>/', testt),
-]
--- wiki/views.py
+++ wiki/views.py
@@ -1,9 +1,1 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-
-def test(request):
-    return HttpResponse("테스트")
-    
-
-def testt(request, id):
-    return HttpResponse("테스트22")
