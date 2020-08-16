e715d7e3fbc8937e1fefc939d33e82c2b8ef4c69
iam-dante <koreanhoseon@gmail.com>
2020-08-16 17:11:36 +0900

    Create, Read 기능 추가

--- wiki/views.py
+++ wiki/views.py
@@ -22,17 +22,26 @@ class Write(View):
             return HttpResponse('이미 존재하는 용어입니다.', status=400)
 
         term = TermItem.objects.create(term=term, description=description)
-        return redirect('/terms/{}'.format(term.pk))
+        return redirect('/terms/{}'.format(term.term_id))
 
 class Detail(View):
+
     def get(self, request, *args, **kwargs):
-        # print(kwargs.get('id'))
-        return render(request, 'wiki/detail.html')
+        try:
+            term = Term.objects.get(id=kwargs.get('id'))
+        except Term.DoesNotExist:
+            return HttpResponse('존재하지 않는 아이디입니다.', status=404)
+
+        term_item = TermItem.objects.filter(term=term).order_by('-created_at').first()
+        return render(request, 'wiki/detail.html', {
+            'term': term,
+            'term_item': term_item,
+        })
 
 class Edit(View):
+
     def get(self, request, *args, **kwargs):
         return render(request, 'wiki/edit.html')
 
     def post(self, request, *args, **kwargs):
-        print(args, kwargs)
         return render(request, 'wiki/detail.html')
