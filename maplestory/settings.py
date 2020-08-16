e6cd0de696b7a354a61290fbad296be356c0f11e
well-balanced <ws.kim@specupad.com>
2020-08-16 18:21:51 +0900

    Modify settings.py

--- maplestory/settings.py
+++ maplestory/settings.py
@@ -80,8 +80,12 @@ WSGI_APPLICATION = 'maplestory.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'maplestory',
+        'USER': 'root',
+        'PASSWORD': '1234',
+        'HOST': 'db',
+        'PORT': 3306,
     }
 }
 
