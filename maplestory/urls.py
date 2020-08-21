4f1bb0209badbcd05b660124fc81a5779e3fceb5
iam-dante <koreanhoseon@gmail.com>
2020-08-21 21:46:04 +0900

    네이밍 수정

--- maplestory/urls.py
+++ maplestory/urls.py
@@ -15,10 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from .views import Index
+from .views import IndexView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', Index.as_view(), name='index'),
+    path('', IndexView.as_view(), name='index'),
     path('terms/', include('wiki.urls')),
 ]
--- maplestory/views.py
+++ maplestory/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 from wiki.models import Term, TermRevision
 from django.views import View
 
-class Index(View):
+class IndexView(View):
     def get(self, request, *args, **kwargs):
         terms = Term.objects.all()
         return render(request, 'index.html', {
