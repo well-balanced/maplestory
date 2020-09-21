34b9de379d3c2cf59ffd60a2dd59f21cf9f6607a
iam-dante <koreanhoseon@gmail.com>
2020-09-21 15:38:01 +0900

    네이밍 수정

--- wiki/views.py
+++ wiki/views.py
@@ -43,7 +43,8 @@ class EditView(View):
     def get(self, request, *args, **kwargs):
         page_id = kwargs.get('id')
         term_name = Term.objects.get(id=page_id).name
-        description = TermRevision.objects.filter(term_id=page_id).order_by('-created_at').first().description
+        term_pointer = TermPointer.objects.filter(term_id=page_id).get().term_revision_id
+        description = TermRevision.objects.filter(term_id=page_id).get(pk=term_pointer).description
         return render(request, 'wiki/edit.html', {
             'id': page_id,
             'term': term_name,
@@ -54,9 +55,7 @@ class EditView(View):
         page_id = kwargs.get('id')
         description = request.POST.get('description', '')
         term_revision = TermRevision.objects.create(description=description, term_id=page_id)
-        term_revision.save()
-        if term_revision:
-            TermPointer.objects.filter(term_id=page_id).update(term_revision_id=term_revision.id)
+        TermPointer.objects.filter(term_id=page_id).update(term_revision_id=term_revision.id)
         return redirect('/terms/{}'.format(page_id))
 
 class HistoryView(View):
@@ -66,16 +65,16 @@ class HistoryView(View):
             term = Term.objects.get(id=kwargs.get('id'))
         except Term.DoesNotExist:
             return HttpResponse('존재하지 않는 아이디입니다.', status=404)
-        history = TermRevision.objects.filter(term=term).order_by('-created_at')
+        revisions = TermRevision.objects.filter(term=term).order_by('-created_at')
         return render(request, 'wiki/history.html', {
             'term': term,
-            'history': history,
+            'history': revisions,
             'id': term.id,
         })
     
     def post(self, request, *args, **kwargs):
         term_id = kwargs.get('id')
-        post_term_revision_id = request.POST.get('test', '')
+        post_term_revision_id = request.POST.get('return_revision', '')
         if post_term_revision_id:
             TermPointer.objects.filter(term_id=term_id).update(term_revision_id=post_term_revision_id)
         return redirect('/terms/{}'.format(term_id))
