44169e048ee1d13c62b6f51fa9443e6f18677661
well-balanced <ws.kim@specupad.com>
2021-02-15 23:22:29 +0900

    Update the settings_prod.py

--- maplestory/settings_prod.py
+++ maplestory/settings_prod.py
@@ -2,24 +2,19 @@ from maplestory.settings import *
 
 DEBUG = False
 
-# db_name = os.getenv('RDS_NAME')
-# db_user = os.getenv('RDS_USER')
-# db_password = os.getenv('MYSQL_DB_PASSWORD')
-# db_host = os.getenv('RDS_HOST')
-# db_port = os.getenv('RDS_PORT')
+db_name = os.getenv('DB_NAME', 'showmethatcode')
+db_user = os.getenv('DB_USER')
+db_password = os.getenv('DB_PASSWORD')
+db_host = os.getenv('DB_HOST')
+db_port = os.getenv('DB_PORT', 3306)
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.mysql',
-#         'NAME': db_name,
-#         'USER': db_user,
-#         'PASSWORD': db_password,
-#         'HOST': db_host,
-#         'PORT': db_port,
-#         'OPTIONS': {
-#             'init_command': "SET sql_mode='STRICT_TRANS_TABLES'; SET foreign_key_checks = 0;",
-#             'charset': 'utf8mb4',
-#             'use_unicode': True,
-#         },
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': db_name,
+        'USER': db_user,
+        'PASSWORD': db_password,
+        'HOST': db_host,
+        'PORT': db_port,
+    }
+}
