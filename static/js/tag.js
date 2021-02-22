807f1a2e10bd474f63878dbc0f615ff73cb69c8f
박상민 <iamsangminpark@gmail.com>
2021-02-22 17:02:53 +0900

    add markdown editor

--- static/js/tag.js
+++ static/js/tag.js
@@ -1,83 +1,35 @@
 // CSRF TOKEN 함수
 function getCookie(name) {
-    let cookieValue = null;
-    if (document.cookie && document.cookie !== '') {
-        const cookies = document.cookie.split(';');
-        for (let i = 0; i < cookies.length; i++) {
-            const cookie = cookies[i].trim();
-            if (cookie.substring(0, name.length + 1) === (name + '=')) {
-                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
-                break;
-            }
-        }
+  let cookieValue = null;
+  if (document.cookie && document.cookie !== "") {
+    const cookies = document.cookie.split(";");
+    for (let i = 0; i < cookies.length; i++) {
+      const cookie = cookies[i].trim();
+      if (cookie.substring(0, name.length + 1) === name + "=") {
+        cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+        break;
+      }
     }
-    return cookieValue;
+  }
+  return cookieValue;
 }
+const csrftoken = getCookie("csrftoken");
 
-const csrftoken = getCookie('csrftoken');
+/* Markdown editor */
+const simplemde = new SimpleMDE({ element: document.getElementById("MyID") });
 
-// var elements = document.getElementById("element");
-// var taskSubmit = document.getElementById("btn_add_task");
-// var taskBox = document.querySelector("#text_task");
-// var taskList = document.getElementById("list_tasks");
-// var taskLi = document.querySelectorAll("ul li");
-
-// taskBox.addEventListener("keyup", removeSpecial);
-// taskSubmit.addEventListener("click", removeSpecial);
-
-// function removeSpecial(e) {
-//     e.target.value = e.target.value.replace(/[^ㄱ-힣a-zA-Z0-9+#]/gi, "");
-// }
-
-// let checkSame = [];
-// taskBox.addEventListener("keyup", function (e) {
-//     const keyCode = e.keyCode;
-//     if (e.keyCode == 188 || e.keyCode == 32 || e.keyCode == 13) {
-//         addTag();
-//     }
-// });
-// taskSubmit.addEventListener("click", addTag, false);
-
-// function addTag() {
-//     const task = taskBox.value.trim().toLowerCase();
-//     const newLi = document.createElement("li");
-//     const input = document.createElement("input");
-//     input.type = "hidden";
-//     input.value = task;
-//     input.name = "tagList";
-//     const removeBtn = document.createElement("button");
-//     const element = newLi.appendChild(document.createTextNode(task));
-//     if (taskBox.value != "" && checkSame.includes(task) === false) {
-//         checkSame.push(task);
-//         taskList.appendChild(newLi);
-//         newLi.appendChild(input);
-//         newLi.appendChild(removeBtn);
-//         removeBtn.innerHTML = "X";
-//         taskBox.value = "";
-//         removeBtn.addEventListener("click", function () {
-//             removeBtn.parentNode.removeChild(removeBtn);
-//             newLi.parentNode.removeChild(newLi);
-//             checkSame.splice(checkSame.indexOf(task), 1);
-//         });
-//     } else if (checkSame.includes(task) === true) {
-//         taskBox.value = "";
-//         alert("중복된 연관어입니다.");
-//     }
-// }
-
-// var elements = document.getElementById("element");
-// var taskSubmit = document.getElementById("btn_add_task");
-// var taskBox = document.querySelector("#text_task");
-// var taskList = document.getElementById("list_tasks");
-// var taskLi = document.querySelectorAll("ul li");
 const termSubmit = document.getElementById("btn_add_task");
 const termList = document.getElementById("list_tasks");
 const termBox = document.getElementById("text_task");
 const termLi = document.querySelectorAll("ul li");
 
-termSubmit.addEventListener("click", () => {
-  addRelatedTerm(termBox.value);
-}, false);
+termSubmit.addEventListener(
+  "click",
+  () => {
+    addRelatedTerm(termBox.value);
+  },
+  false
+);
 
 termBox.addEventListener("keyup", (e) => {
   const keyCode = e.keyCode;
@@ -102,22 +54,22 @@ function addRelatedTerm(name) {
   liElem.innerText = trimmedTerm;
 
   const removeBtn = document.createElement("button");
-  removeBtn.innerText = 'X';
-  removeBtn.addEventListener('click', () => {
+  removeBtn.innerText = "X";
+  removeBtn.addEventListener("click", () => {
     removeBtn.parentNode.removeChild(removeBtn);
     liElem.parentNode.removeChild(liElem);
-  })
+  });
 
   const items = document.querySelectorAll("#list_terms > li");
   for (let i = 0; i < items.length; i++) {
-    if (items[i].innerHTML.split('<')[0] === newTerm) {
-      alert('중복 단어');
-      termBox.value = '';
+    if (items[i].innerHTML.split("<")[0] === newTerm) {
+      alert("중복 단어");
+      termBox.value = "";
       return;
     }
-  };
+  }
   termList.appendChild(liElem).appendChild(removeBtn);
   liElem.appendChild(input);
-  termBox.value = '';
+  termBox.value = "";
   return;
 }
